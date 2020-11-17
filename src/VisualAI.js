5367370d2232bbaa24f82125e4737e01b44785e5
diberry <diberry@microsoft.com>
2020-11-16 21:39:01 -0800

    add secrets to action

--- src/VisualAI.js
+++ src/VisualAI.js
@@ -93,5 +93,4 @@ const readTextFromURL = async (client, url) => {
     // Return the first page of result. 
     // Replace[0] with the desired page if this is a multi-page file such as .pdf or.tiff.
     return result.analyzeResult; 
-    
 }
